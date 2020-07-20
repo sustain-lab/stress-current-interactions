a8839cd4498bb1b518b00b7fba7b2818ccd3f5c8
Milan Curcic <caomaco@gmail.com>
2020-07-20 18:13:39 -0400

    add jonswap notebook

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # stress-current-interactions
 
 Exploring the interactions between stress, waves, and currents.
+
+## Notebooks
+
+* [Current effects on stress based on JONSWAP spectra and Jeffreys wind input](jonswap.ipynb)
--- stress_current_interactions/diagnostics.py
+++ stress_current_interactions/diagnostics.py
@@ -33,8 +33,8 @@ def dominant_wave_period(wavenumber_spectrum, frequency):
     return np.sum(wavenumber_spectrum**4) / np.sum(wavenumber_spectrum**4 * frequency)
 
 
-def form_drag(source_input, wavenumber_spectrum, phase_speed, \
+def form_drag(source_input, wavenumber_spectrum, phase_speed, wavenumber, \
               water_density=1e3, grav=9.8):
-    dk = np.zeros(k.shape)
-    dk[1:] = np.diff(k)
+    dk = np.zeros(wavenumber.shape)
+    dk[1:] = np.diff(wavenumber)
     return water_density * grav * np.sum(source_input * wavenumber_spectrum * dk / phase_speed)
